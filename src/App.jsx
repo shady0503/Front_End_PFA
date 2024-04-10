eac2eb01c01efa4e7e647894bcb184a591f1b6a2
Shady0503 <chaditaqi3@gmail.com>
2024-04-10 02:42:33 +0000

    hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from './Componants/allpages/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Componants/allpages/Footer';
@@ -12,22 +11,60 @@ import { useScrollToTop } from './useScrollTop';
 import Cart from './Componants/allpages/Cart';
 import { DataProvider } from './Componants/dataContext';
 import DashBoard from './Componants/DashBoard/Dashboard';
+import { useEffect, useState } from 'react';
+import MainNavbar from './Componants/allpages/MainNavbar';
 
 
 function App() {
   const location = useLocation();
   const showHeaderAndFooter = !location.pathname.startsWith('/Front_End_PFA/Dashboard');
 
+  const [open, setOpen] = useState(false)
+  useEffect(() => {
+    const threshold = 1250;
+    let previousWidth = window.innerWidth;
+
+    const handleResize = () => {
+      const currentWidth = window.innerWidth;
+      // Opening condition: if previous width was > 900 and now it's <= 900
+      if (previousWidth > threshold && currentWidth <= threshold) {
+        setOpen(true);
+      }
+      // Closing condition: if previous width was <= 900 and now it's > 900
+      else if (previousWidth <= threshold && currentWidth > threshold) {
+        setOpen(false);
+      }
+      // Update previousWidth for the next event
+      previousWidth = currentWidth;
+    };
+
+    // Initial check to set the correct state
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   useScrollToTop()
   return (
     <DataProvider>
       <div className='App-Container d-flex flex-column'>
-        {showHeaderAndFooter && <Header />}
+        {showHeaderAndFooter && (
+              <div className='header-container' style={{width : window.innerWidth > 900 || open ? '100%' : '0%'}}>
+              {(open && <div className='hamberger-container'  onClick={()=>{setOpen(!open)}}>
+                <div className='hamberger first'></div>
+                <div className='hamberger second'></div>
+                <div className='hamberger third'></div>
+              </div>)}
+              {(!open && <MainNavbar setOpen={setOpen} />)}
+            </div>
+        )}
         <Routes>
             <Route path="/Front_End_PFA/cart" element={<Cart />} />
             <Route path="/Front_End_PFA/LandingPage" element={<LandingPage />} />
             <Route path="/Front_End_PFA/" element={<HomePage />} />
-            <Route path="/Front_End_PFA/login" element={<LogIn />} />
+            <Route path="/Front_End_PFA/home" element={<HomePage />} />
+            <Route path="/Front_End_PFA/Login" element={<LogIn />} />
             <Route path="/Front_End_PFA/laptops" element={<Laptops filter="True" />} />
             <Route path="/Front_End_PFA/Phones" element={<Laptops filter="True" />} />
             <Route path="/Front_End_PFA/Accessories" element={<Laptops filter="True" />} />
---
+++ src/Componants/allpages/MainNavbar.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.webp';
+import './MainNavbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+
+
+
+export default function MainNavbar({ setOpen }) {
+    const menu = ['home', 'laptops', 'phones', 'accessories', 'Deals', 'support', 'Log In']
+    return (
+        <div className='navbar'>
+            <span onClick={() => { setOpen(true); console.log('false') }}>X</span>
+            <div className='logo-container'>
+                <img src={logo} alt="" className='logo' />
+                <h6>Republic Of Gamers</h6>
+            </div>
+
+            <div className='main-navbar'>
+                <ul className='navbar-nav'>
+                    {menu.map(item => {
+                        return (
+                            <li className='navbar-item'>
+                                <Link to={`/Front_End_PFA/${item.replace(/\s+/g, '')}`} className='navbar-link'>{item}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+
+            <div className="second-navbar">
+                <ul className='navbar-nav'>
+                    <li className='navbar-item'><Link><FontAwesomeIcon className="icn" icon={faQuestion} /></Link></li>
+                    <li className='navbar-item'><Link to="/Front_End_PFA/Cart"><FontAwesomeIcon className="icn" icon={faCartShopping}></FontAwesomeIcon></Link></li>
+                    <li className='navbar-item'><Link to="/Front_End_PFA/Dashboard"><FontAwesomeIcon className="icn" icon={faUser} /></Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
